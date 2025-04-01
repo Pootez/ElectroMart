e35d7a651619e01b99abc9c880b23e9fe0124b01
Sondre Espeland <sondrenoe@gmail.com>
2025-04-01 15:49:27 +0200

    Implement basic routing & install tailwind
    Basic routing to pages with 404 redirect.

---
+++ frontend/src/router.tsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from 'react-router'
+import { HomePage } from './pages/HomePage'
+import { NotFoundPage } from './pages/NotFoundPage'
+import { LoginPage } from './pages/LoginPage'
+import { CheckoutPage } from './pages/CheckoutPage'
+import { AdminPage } from './pages/AdminPage'
+import { ProfilePage } from './pages/ProfilePage'
+import { RegisterPage } from './pages/RegisterPage'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <HomePage />,
+        errorElement: <NotFoundPage />
+    },
+    {
+        path: '/login',
+        element: <LoginPage />,
+    },
+    {
+        path: '/register',
+        element: <RegisterPage />,
+    },
+    {
+        path: '/admin',
+        element: <AdminPage />,
+    },
+    {
+        path: '/checkout',
+        element: <CheckoutPage />,
+    },
+    {
+        path: '/profile',
+        element: <ProfilePage />,
+    }
+])
+
+export default router
